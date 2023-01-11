33b6f17ffb5ad37dd3f4510229fc112b97fe779c
Daniel Bray <daniel.bray.dev@hotmail.com>
2023-01-11 21:18:27 +0000

    Add controller ItemCreatePost

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
-let express = require("express");
 let async = require("async");
+const { body, validationResult, check } = require("express-validator");
+const { response } = require("../app");
 
 const Category = require("../models/Category");
 const Item = require("../models/Item");
@@ -49,14 +50,97 @@ exports.itemCreateGet = (req, res, next) => {
       res.render("itemCreate", {
         units: units,
         categories: categories,
+        err: req.body.err,
       });
     }
   );
 };
 
-exports.itemCreatePost = (req, res, next) => {
-  res.send(req.params + "itemCreatePost");
-};
+exports.itemCreatePost = [
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Please insert a name."),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Please insert a description of the item"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price should be a number")
+    .trim()
+    .escape(),
+  body("quantity")
+    .isNumeric()
+    .withMessage("Quantity should be a number")
+    .trim()
+    .escape(),
+  check("category").exists().withMessage("Please select a category"),
+  check("unit").exists().withMessage("Please select a unit"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.body.err = errors.errors;
+      console.log(req.body.err);
+      return this.itemCreateGet(req, res, next);
+    }
+    let item = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      weight: {
+        unit: req.body.unit,
+        quantity: req.body.quantity,
+      },
+    };
+    async.parallel(
+      {
+        unitsDocuments(cb) {
+          Item.aggregate([
+            {
+              $group: {
+                _id: "$weight.unit",
+              },
+            },
+          ]).exec(cb);
+        },
+        categoriesDocuments(cb) {
+          Category.find({}, { name: 1 }).exec(cb);
+        },
+      },
+      async function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        let possibleCategories = results.categoriesDocuments.map(
+          (document) => document.name
+        );
+        let possibleUnits = results.unitsDocuments.map(
+          (document) => document._id
+        );
+
+        if (!possibleCategories.find((document) => document == item.category)) {
+          next("Category does't exist");
+        }
+        if (!possibleUnits.find((document) => document == item.weight.unit)) {
+          next("Unit does't exist");
+        }
+        item.category = results.categoriesDocuments.find(
+          (document) => document.name == item.category
+        )._id;
+        let newItem = new Item(item);
+        await newItem.save();
+        let foundItem = await Item.findById(newItem._id).populate("category");
+        console.log(foundItem);
+        res.redirect(foundItem.url);
+      }
+    );
+  },
+];
 
 exports.itemDeleteGet = (req, res, next) => {
   Item.findOne({ _id: req.params.id })
