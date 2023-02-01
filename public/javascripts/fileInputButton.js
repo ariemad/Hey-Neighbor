de27f6cadc3f22271fece4adafbfb7eaf925db87
Daniel Bray <daniel.bray.dev@hotmail.com>
2023-02-01 18:30:58 +0000

    Style Item, Update and Delete views

---
+++ public/javascripts/fileInputButton.js
@@ -0,0 +1,6 @@
+const fileInputBtn = document.getElementById("fileInputBtn");
+const fileInput = document.getElementById("image-input");
+
+fileInputBtn.addEventListener("click", function () {
+  fileInput.click();
+});
